e40f29f46a81bf6df935a0ea6fd1650a1e1c9bfc
Szymonixo <67418724+Szymonixo@users.noreply.github.com>
2026-05-21 09:20:54 +0200

    Create ClientIssue.cs

---
+++ SOK-WPF/Models/ClientIssue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOK_WPF.Models
+{
+    public class ClientIssue
+    {
+        public int Id { get; set; }
+
+        public int TopicId { get; set; }
+
+        public int ClientId { get; set; }
+
+        public int OrderId { get; set; }
+
+        public DateOnly IssuedOn { get; set; }
+        public int[]? ProductsIds { get; set; }
+        public string? Description { get; set; }
+        public string? Expectations { get; set; }
+        
+    }
+}
