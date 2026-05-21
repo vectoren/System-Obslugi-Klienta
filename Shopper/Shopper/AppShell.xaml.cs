4761c5ba278c1a6e0a3e9e0a0486a092102e05ce
vectoren <cieslabartek721@gmail.com>
2026-05-21 09:16:16 +0200

    MAUI app initialization

---
+++ Shopper/Shopper/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Shopper
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Shopper/Shopper/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Shopper
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
