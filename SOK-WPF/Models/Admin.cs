0870aaf159bf99311977ffd71deaceb9bd83f3d8
Szymonixo <67418724+Szymonixo@users.noreply.github.com>
2026-05-21 09:25:18 +0200

    Create Admin.cs

---
+++ SOK-WPF/Models/Admin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOK_WPF.Models
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Role {  get; set; }
+        public string? Email { get; set; }
+
+    }
+}
