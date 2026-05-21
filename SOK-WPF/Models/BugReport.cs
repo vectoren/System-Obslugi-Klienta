21b44561fb28306d372ef80d446a93a073f5d295
Szymonixo <67418724+Szymonixo@users.noreply.github.com>
2026-05-21 09:23:48 +0200

    Create BugReport.cs

---
+++ SOK-WPF/Models/BugReport.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOK_WPF.Models
+{
+    public class BugReport
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
