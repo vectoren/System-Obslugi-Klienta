4170e730a2049696a198c2aa1bef58d1d63fb223
Szymonixo <szymonixo007@gmail.com>
2026-05-25 19:54:43 +0200

    Dashboard made!

---
+++ SOK-WPF/ViewModels/DashboardVM.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using SOK_WPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOK_WPF.ViewModels
+{
+    [ObservableObject]
+    public partial class DashboradVM
+    {
+        [ObservableProperty]
+        public List<BugReport> bugReports = new();
+        public DashboradVM()
+        {
+        }
+    }
+}
