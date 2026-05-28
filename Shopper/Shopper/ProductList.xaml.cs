ece37d74d602bacdb9aceb4c304e250a8b9aa496
TengokuuU <skajugranie@gmail.com>
2026-05-28 08:20:55 +0200

    Dodanie obsługi pobierania produktów i szkieletu zdarzeń
    Dodano metodę asynchroniczną do pobierania produktów z serwisu REST oraz obsługę błędów. Utworzono szkielet metod obsługujących zdarzenia związane z listą produktów. Dodano odpowiednie przestrzenie nazw i pole serwisu REST.

--- Shopper/Shopper/ProductList.xaml.cs
+++ Shopper/Shopper/ProductList.xaml.cs
@@ -1,3 +1,4 @@
+using Shopper.Models;
 using Shopper.Services;
 
 namespace Shopper;
@@ -5,10 +6,36 @@ namespace Shopper;
 public partial class ProductList : ContentPage
 {
     private readonly RestService _service = new RestService();
-    public ProductList()
+	public ProductList()
 	{
 		InitializeComponent();
 	}
 
-	
+    private async void PobierzDane(object sender, EventArgs e)
+    {
+        try
+        {
+            List <Product> dane = await _service.GetProductsAsync();
+            ProductsCollectionView.ItemsSource = dane;
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Błąd połączenia", $"Nie udało się pobrać danych: {ex.Message}", "OK");
+        }
+    }
+
+    private void OnProductItemTapped(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void OnRefreshViewRefreshing(object sender, EventArgs e)
+    {
+
+    }
+
+    private void OnSearchBarTextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
 }
