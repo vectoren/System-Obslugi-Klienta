20bbeca42f1c5241b3c521cd775640419c96c8f9
TengokuuU <skajugranie@gmail.com>
2026-05-28 14:53:13 +0200

    Dodaj klasę Account do Shopper.Models
    Dodano plik Account.cs z klasą Account, zawierającą właściwości id, fname, lname, email oraz password do przechowywania danych użytkownika. Uwzględniono wymagane usingi.

---
+++ Shopper/Shopper/Models/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shopper.Models
+{
+    public class Account
+    {
+        //fnaem, lname, email, password, registerdate
+        public int id { get; set; }
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+
+
+    }
+}
