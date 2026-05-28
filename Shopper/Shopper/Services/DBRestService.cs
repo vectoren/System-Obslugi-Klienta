67a9ca71a8cb8bb5f8e60682a38fdc41275c4d74
Hiumpik-err <kamil.jendrusz@gmail.com>
2026-05-28 20:16:47 +0200

    registration

---
+++ Shopper/Shopper/Services/DBRestService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Shopper.Services
+{
+    public static class DBRestService
+    {
+        private static HttpClient httpClient;
+        private static CookieContainer cookieContainer;
+
+        private static void GetClient()
+        {
+            if(httpClient is null)
+            {
+                cookieContainer = new CookieContainer();
+                var handler = new HttpClientHandler 
+                {
+                    CookieContainer = cookieContainer,
+                    UseCookies = true
+                };
+            }
+        }
+    }
+}
