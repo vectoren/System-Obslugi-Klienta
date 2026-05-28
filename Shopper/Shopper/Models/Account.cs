b223af3dcc79773d36f8503572cb5ffec771891a
vectoren <cieslabartek721@gmail.com>
2026-05-28 15:07:52 +0200

    Dodano rejestrację użytkownika
    Zakomentowano zakładki w `AppShell.xaml` i dodano `RegisterAccountPage`. Zaktualizowano `Account.cs`, usuwając `id` i dodając konstruktory. W `RegisterAccountPage.xaml` dodano identyfikatory pól i obsługę zdarzenia rejestracji. W `RegisterAccountPage.xaml.cs` dodano metody `GoLogin` i `Register` z obsługą walidacji oraz wysyłania danych na serwer.

--- Shopper/Shopper/Models/Account.cs
+++ Shopper/Shopper/Models/Account.cs
@@ -7,11 +7,21 @@ namespace Shopper.Models
     public class Account
     {
         //fnaem, lname, email, password, registerdate
-        public int id { get; set; }
+        //public int id { get; set; }
         public string fname { get; set; }
         public string lname { get; set; }
         public string email { get; set; }
         public string password { get; set; }
+
+        public Account() { }
+        public Account(string fname, string lname, string email, string password)
+        {
+            //this.id = id;
+            this.fname = fname;
+            this.lname = lname;
+            this.email = email;
+            this.password = password;
+        }
 
 
     }
--- Shopper/Shopper/Views/RegisterAccountPage.xaml.cs
+++ Shopper/Shopper/Views/RegisterAccountPage.xaml.cs
@@ -1,4 +1,5 @@
 using Shopper.Models;
+using System.Net.Http.Json;
 
 namespace Shopper.Views;
 
@@ -9,6 +10,8 @@ public partial class RegisterAccountPage : ContentPage
 		InitializeComponent();
 	}
 
+    
+
     private async void GoLogin(object sender, EventArgs e)
     {
         if (sender is Button button )
@@ -16,5 +19,30 @@ public partial class RegisterAccountPage : ContentPage
 
             await Navigation.PushAsync(new LoginPage());
         }
+    }
+
+    private async void Register(object sender, EventArgs e)
+    {
+        if(String.IsNullOrEmpty(fname.Text) || String.IsNullOrEmpty(lname.Text) || String.IsNullOrEmpty(email.Text) || String.IsNullOrEmpty(password.Text))
+        {
+            await DisplayAlertAsync("ERROR", "Proszę wypełnić wszystkie pola.", "OK");
+            return;
+        }
+        Account newAccount = new Account(fname.Text, lname.Text, email.Text, password.Text);
+        // Tutaj możesz dodać logikę do zapisania nowego konta, np. wysłanie danych do serwera lub zapisanie lokalnie
+        await DisplayAlertAsync("SUKCES", "Konto zostało zarejestrowane.", "OK");
+        HttpClient client = new HttpClient();
+        client.BaseAddress = new Uri("http://10.0.2.2:8080/");
+        var response = await client.PostAsJsonAsync("api/register", newAccount);
+        if(response.IsSuccessStatusCode)
+        {
+            await DisplayAlertAsync("SUKCES", "Konto zostało zarejestrowane na serwerze.", "OK");
+        }
+        else
+        {
+            await DisplayAlertAsync("BŁĄD", "Nie udało się zarejestrować konta na serwerze.", "OK");
+        }
+
+
     }
 }
