ded5a47ed3c449f135a31024efc0de1a1240dc5e
Szymonixo <szymonixo007@gmail.com>
2026-06-10 21:33:18 +0200

    dummy chat function ^^

---
+++ SOK-WPF/Views/ChatUC.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SOK_WPF.Views
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ChatUC.xaml
+    /// </summary>
+    public partial class ChatUC : UserControl
+    {
+        public ChatUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
